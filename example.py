63c094d23bc4d8e751f793865e2370363a6d7d63
WaffleMixer <mohammad.sakib0194@gmail.com>
2022-11-14 19:59:36 +0600

    Update

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ async def my_event_handler(event):
     user_id = event.message.from_id.user_id
     
     # the \u200b is a unicode which is not visible on Telegram clients and makes the invisible tagging possible
-    await client.send_message("alrighy", f"Hidden tagging {user_id}[\u200b](tg://user?id={user_id})")
+    await client.send_message("group_name", f"Hidden tagging {user_id}[\u200b](tg://user?id={user_id})")
 
 client.start()
 client.run_until_disconnected()
